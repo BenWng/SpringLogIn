39ee05dbd50a44e10882e79b9dcc739b4a08d79d
BenWng <wnghan@gmail.com>
2016-07-10 01:15:03 -0700

    UserDaoImpl now also implements UserDetailService, not sure if everything in UserrDaoImpl is correct

--- src/main/java/com/login/UserrDAOImpl.java
+++ src/main/java/com/login/UserrDAOImpl.java
@@ -1,17 +1,18 @@
 package com.login;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-
 import org.springframework.stereotype.Component;
 
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Ben_Big on 7/8/16.
@@ -19,18 +20,53 @@ import java.util.List;
 
 
 @Component
-public class UserrDAOImpl implements UserrDAO {
+public class UserrDAOImpl implements UserrDAO, UserDetailsService {
 
     @PersistenceContext
     private EntityManager entityManager;
-    /*
+
     @Override
-    public UserDetails loadUserByUserName(String name)
+    public UserDetails loadUserByUsername(String name)
     throws UsernameNotFoundException
     {
-        GrantedAuthorityImpl
+        Userr u=null;
+        Query query=entityManager.createQuery("select u from Userr u where u.name=:N");
+        query.setParameter("N",name);
+        List<Userr> userrList=query.getResultList();
+        Iterator<Userr> itr=userrList.iterator();
+        if(itr.hasNext()){
+            u=itr.next();
+        }
+        else{
+            throw new UsernameNotFoundException("No such user:"+name);
+        }
 
-    }*/
+        // boolean fields required for User (the one in security.core.userdetails.User)
+        boolean accountNonExpired=true;
+        boolean credentialNonExpired=true;
+        boolean accountNonLocked=true;
+        boolean accountIsEnabled=true;
+
+        return new User(
+                u.getName(),
+                u.getPassword(),
+                accountIsEnabled,
+                accountNonExpired,
+                credentialNonExpired,
+                accountNonLocked,
+                getAuthorities()
+                );
+
+    }
+
+    //
+    public Collection<? extends GrantedAuthority> getAuthorities(){
+        List<GrantedAuthority> authList=new ArrayList<>();
+        authList.add(new SimpleGrantedAuthority("user"));
+        return authList;
+    }
+
+
 
     @Override
     public void persist(Userr u){
