f22e712330ef4caa01ac5052a33462e449a76247
ca16 <chloeanastasiades@gmail.com>
2016-07-14 12:21:12 -0700

    added basic testing and updated Readme

--- src/main/java/com/login/SecurityJavaConfig.java
+++ src/main/java/com/login/SecurityJavaConfig.java
@@ -1,6 +1,5 @@
 package com.login;
 
-import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
